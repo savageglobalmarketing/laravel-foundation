2c0f1c466f42ad697779c6c6319a94d59fb190aa
Marcelo Barros da Silva <maxcelos@outlook.com>
2021-01-12 11:50:04 -0300

    Enabling upload endpoint

--- Http/Controllers/UploadController.php
+++ Http/Controllers/UploadController.php
@@ -10,10 +10,8 @@ class UploadController extends Controller
 {
     public function upload(Request $request)
     {
-        dd($request->all());
         $file = Storage::disk(config('foundation.upload_disk'))->put('uploads', $request->file('uploadFileObj'));
 
-        dd($file);
         return response()->json(['path' => $file]);
     }
 }
